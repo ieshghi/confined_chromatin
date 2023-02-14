ced906813d6fcf4a642fce3193ad37f49824dd80
Iraj Eshghi <ie355@gr043.nyu.cluster>
2023-02-13 22:36:36 -0500

    corner blob still evolves strangely

--- scripts/corner_blob.py
+++ scripts/corner_blob.py
@@ -43,7 +43,7 @@ X = AR*np.cos(PH)*np.sqrt(1-CO**2)
 Y = AR*np.sin(PH)*np.sqrt(1-CO**2)
 Z = AR*CO
 
-rho_init = np.exp(-((X)**2 + Y**2 + Z**2)/0.04)
+rho_init = np.exp(-((X-0.3)**2 + Y**2 + Z**2)/0.04)
 
 sh.nmax = nmax
 #rho_init = np.exp(-(AR-0.5)**2/(0.1)**2)*np.exp(-CO**2/0.1**2)*np.exp(-(PH-np.pi)**2/0.01**2)
@@ -71,6 +71,6 @@ initarrs = (wr,wth,wphi,mr,mth,mphi,sh)
 wa,wb,ma,mb,sh,r = spherical_integrate.main(simpars,physpars,initarrs,sh,zers,p)
 #spherical_integrate.save_out(wa,wb,ma,mb,sh,r,simpars,'corner_blob')
 #wa,wb,ma,mb,sh,r,simpars = plotting_utils.load_w_hist('corner_blob')
-rho_hist = plotting_utils.density_movie(wb,sh,r,simpars,rho_init,phi0,zers,undersamp = 1,name = 'density_mov_centered',minmax = [np.min(rho_init),np.max(rho_init)],pool = p)
+rho_hist = plotting_utils.density_movie(wb,sh,r,simpars,rho_init,phi0,zers,undersamp = 1,name = 'density_mov_offcenter',minmax = [np.min(rho_init),np.max(rho_init)],pool = p)
 #plotting_utils.animate_soln(wa,wb,ma,mb,sh,r,simpars,zers,undersamp = 1,fname = 'diff_mov_centered')
 #plotting_utils.animate_soln_arrows(wa,wb,sh,r,simpars,zers,undersamp = 1,fname = 'diffusive_arrows',spatial_undersamp = 10)
