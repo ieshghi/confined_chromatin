all modes with m>0 had a factor of 2 off. fixed that, and now all evolutions are as expected.

scripts/center_blob.py
@@ -42,7 +42,7 @@ wr,wth,wphi,mr,mth,mphi = sim_utils.initialize_arrays(simpars,sh)
 X = AR*np.cos(PH)*np.sqrt(1-CO**2)
 Y = AR*np.sin(PH)*np.sqrt(1-CO**2)
 Z = AR*CO
-x0 = 0.51
+x0 = -0.41
 y0 = 0
 z0 = 0
 name = 'density_mov_xshift01'
@@ -75,7 +75,7 @@ expected_coeffs = gradphi_to_w[0,:]*[(-1)**m*np.exp(-1/2*m*np.pi*(m*np.pi*sigma*
 wa,wb,ma,mb,sh,r = spherical_integrate.main(simpars,physpars,initarrs,sh,zers,p)
 
 t0 = sigma**2/(4*ld**2)
-tarr = np.linspace(0,1,nt)*dt*nt/2 + t0
+tarr = np.linspace(0,1,nt)*dt*nt + t0
 ARsqshift = (X-x0)**2 + (Y-y0)**2 + (Z-z0)**2
 expected_density = np.exp(-ARsqshift[:,:,:,None]/(8*ld**2*tarr[None,:]))*(t0*np.ones(AR.shape)[:,:,:,None]/tarr[None,:])**(3/2)
 
@@ -89,7 +89,7 @@ plt.close('all')
 ARshift=AR-abs(x0)
 rshift = r-abs(x0)
 
-midslice_r = (CO==np.min(np.abs(CO)))*(PH==np.min(PH))
+midslice_r = (CO==np.min(np.abs(CO)))*(PH==np.pi)
 midslice_p = (CO==np.min(np.abs(CO)))*(ARshift == np.min(np.abs(ARshift)))
 rslice = rshift==np.min(np.abs(rshift))
 rslice_val = r[rslice]
@@ -117,7 +117,7 @@ for i in range(len(slices_plot)):
     ax[2,0].set_title(r'$w_{\phi}$')
     ax[2,0].set_xlabel(r'$\phi$')
 
-    ax[0,1].plot(r,rho_hist[0,:,fr])
+    ax[0,1].plot(r,rho_hist[np.where(phi==np.pi),:,fr][0][0])
     ax[0,1].plot(r,expected_density[midslice_r,fr],'--')
     ax[0,1].set_title(r'$\rho$')
     ax[0,1].set_xlabel('r')
@@ -125,7 +125,7 @@ for i in range(len(slices_plot)):
     wth,wph,wr = sim_utils.my_sh_to_spat(wa[:,:,fr],wb[:,:,fr],sh,simpars,zers,p)
     wr_r = wr[midslice_r]
     ax[1,1].plot(r,wr_r)
-    ax[1,1].plot(r,-2*ld**2/(phi0*(1-phi0))*np.gradient(rho_hist[0,:,fr],r,edge_order=2),'--')
+    ax[1,1].plot(r,-2*ld**2/(phi0*(1-phi0))*np.gradient(rho_hist[np.where(phi==np.pi),:,fr][0][0],r,edge_order=2),'--')
     ax[1,1].set_title(r'$w_r$')
     ax[1,1].set_xlabel('r')
 

scripts/singlemode_evol.py
@@ -19,9 +19,9 @@ mmax = 10
 nmax = 20
 nr = 100
 rmax = 1
-dt = 0.03
-nt = 100
-eps = 0.5
+dt = 0.1
+nt = 200
+eps = .5
 lam = 0.1
 ls = 0.0000001
 ld = 0.1
@@ -29,7 +29,7 @@ iflinear = 1
 
 l_sim = 2
 m_sim = 1
-n_sim = 2
+n_sim = 0
 
 a_init = 1
 b_init = 1
@@ -84,6 +84,10 @@ mth,mphi,mr = sim_utils.my_sh_to_spat(m_anlm,m_bnlm,sh,simpars,zers,p)
 initarrs = (wr,wth,wphi,mr,mth,mphi,sh) 
 
 wa,wb,ma,mb,sh,r = spherical_integrate.main(simpars,physpars,initarrs,sh,zers,p)
+wa_corr = wa[ind_keep,n_sim,:][0]
+wb_corr = wb[ind_keep,n_sim,:][0]
+wa_else = np.sqrt(np.mean(wa**2,axis=(0,1))) - np.abs(wa_corr)/(wa.shape[0]*wa.shape[1])
+wb_else = np.sqrt(np.mean(wb**2,axis=(0,1))) - np.abs(wb_corr)/(wa.shape[0]*wa.shape[1])
 
 tarr = dt*np.array(range(nt))
 a_evol = a_init*np.exp(tarr*gamma_a)
@@ -116,5 +120,26 @@ ax2.set_xlabel('Time')
 ax2.set_ylabel('Mode amplitude')
 ax2.set_title('Longitudinal')
 fig.tight_layout()
-fig.savefig(name)
+
+fig2,(ax20,ax21) = plt.subplots(1,2)
+ax20.plot(tarr,wa_corr,label='Correct mode')
+ax20.plot(tarr,wa_else,'--',label='RMS all other modes')
+
+ax21.plot(tarr,wb_corr,label='Correct mode')
+ax21.plot(tarr,wb_else,'--',label='RMS all other modes')
+ax21.legend()
+ax20.legend()
+
+ax21.set_xlabel('Time')
+ax21.set_ylabel('Mode amplitude')
+ax21.set_title('Longitudinal')
+
+ax20.set_xlabel('Time')
+ax20.set_ylabel('Mode amplitude')
+ax20.set_title('Transverse')
+fig.tight_layout()
+fig2.tight_layout()
+
+
+
 plt.show()

sim_utils.py
@@ -124,13 +124,14 @@ def my_analys(rho,sh,simpars,besselzer,pool = None):
     nmax = simpars.nmax
     nr = len(r)
     el = sh.l
+    em = sh.m
     
     lm_num = len(el)
 
     olm_r = np.array([sh.analys(rho[:,:,i]) for i in range(nr)])
     olm_r[:,0] = np.gradient(olm_r[:,0],r,edge_order=2)
     
-    args = ((r,olm_r[:,i],nmax,el[i],besselzer) for i in range(lm_num))
+    args = ((r,olm_r[:,i],nmax,el[i],em[i],besselzer) for i in range(lm_num))
     if pool is None:
         onlm = np.array(list(map(func2bessel_packed,args)))
     else:
@@ -144,6 +145,7 @@ def my_spat_to_sh(v_th,v_ph,v_r,sh,simpars,besselzer,pool = None): #this routine
     nmax = simpars.nmax
     nr = len(r)
     el = sh.l
+    em = sh.m
     lm_num = len(el)
 
     anlm = np.tile(sh.spec_array(),[nmax,1])
@@ -176,8 +178,8 @@ def my_spat_to_sh(v_th,v_ph,v_r,sh,simpars,besselzer,pool = None): #this routine
         else:
             blm_r[:,i] = blm_r[:,i]*r
 
-    args_a = ((r,alm_r[:,i],nmax,el[i],besselzer) for i in range(lm_num))
-    args_b = ((r,blm_r[:,i],nmax,el[i],besselzer) for i in range(lm_num))
+    args_a = ((r,alm_r[:,i],nmax,el[i],em[i],besselzer) for i in range(lm_num))
+    args_b = ((r,blm_r[:,i],nmax,el[i],em[i],besselzer) for i in range(lm_num))
 
     if pool is None:
         anlm = np.array(list(map(func2bessel_packed,args_a)))
@@ -194,6 +196,7 @@ def my_sh_to_spat(anlm,bnlm,sh,simpars,besselzer = None,pool = None):
     r = simpars.r
     nlat,nphi = sh.set_grid()
     el = sh.l
+    em = sh.m
     lm_num = len(el)
 
     vth = np.empty((nlat,nphi,nr))
@@ -203,8 +206,8 @@ def my_sh_to_spat(anlm,bnlm,sh,simpars,besselzer = None,pool = None):
     one_spat = sh.spat_array()
     two_spat = sh.spat_array()
 
-    args_a = ((r,anlm[i,:],el[i],besselzer) for i in range(lm_num))
-    args_b = ((r,bnlm[i,:],el[i],besselzer) for i in range(lm_num))
+    args_a = ((r,anlm[i,:],el[i],em[i],besselzer) for i in range(lm_num))
+    args_b = ((r,bnlm[i,:],el[i],em[i],besselzer) for i in range(lm_num))
 
     if pool is None:
         alm_r = np.array(list(map(bessel2func_packed,args_a))).T.copy()
@@ -229,26 +232,31 @@ def my_sh_to_spat(anlm,bnlm,sh,simpars,besselzer = None,pool = None):
     return vth,vph,vr
 
 def func2bessel_packed(args):
-    x,y,nmax,l,zers = args
-    return func2bessel(x,y,nmax,l,zers)
+    x,y,nmax,l,m,zers = args
+    return func2bessel(x,y,nmax,l,m,zers)
 
-def func2bessel(x,y,nmax,l,zer):
+def func2bessel(x,y,nmax,l,m,zer):
     x_sc = x/np.max(x)
     lzer = zer[l,:nmax]
     if l > 0:
-        return -2*np.array([simpson(x_sc**2*jn(l,lzer[i]*x_sc)*y,x_sc)/(jn(l-1,lzer[i])*jn(l+1,lzer[i])) for i in range(nmax)])
+        if m>0:
+            return -4*np.array([simpson(x_sc**2*jn(l,lzer[i]*x_sc)*y,x_sc)/(jn(l-1,lzer[i])*jn(l+1,lzer[i])) for i in range(nmax)])
+        else:
+            return -2*np.array([simpson(x_sc**2*jn(l,lzer[i]*x_sc)*y,x_sc)/(jn(l-1,lzer[i])*jn(l+1,lzer[i])) for i in range(nmax)])
     else:
         return np.array([0]+[-2*simpson(x_sc**2*jn(1,lzer[i]*x_sc)*y,x_sc)*(i*np.pi) for i in range(1,nmax)])
         
-
 def bessel2func_packed(args):
-    x,y,l,zers = args
-    return bessel2func(x,y,l,zers)
+    x,y,l,m,zers = args
+    return bessel2func(x,y,l,m,zers)
 
-def bessel2func(x,ncoeffs,l,besselzer):
+def bessel2func(x,ncoeffs,l,m,besselzer):
     nmax = len(ncoeffs)
     lbesselzer = besselzer[l,:nmax]
-    return np.sum(jn(l,x[:,None]*lbesselzer[None,:])*ncoeffs,1)
+    if m>0:
+        return np.sum(jn(l,x[:,None]*lbesselzer[None,:])*ncoeffs,1)/2
+    else:
+        return np.sum(jn(l,x[:,None]*lbesselzer[None,:])*ncoeffs,1)
 
 def genzeros(lmax,mmax):
     zervals = np.zeros((lmax+1,mmax))